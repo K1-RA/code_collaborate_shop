d47d22f04054907a05032539910f400ed6084198
Julia Wos <juliawos22@gmail.com>
2022-04-14 10:09:11 +0200

    fixed index file

--- components/MySecondComponent/index.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./MySecondComponent.module.scss";
-const MySecondComponent = () => {
-  return <div className={styles.title}>This is also a component :0</div>;
-};
-
-export default MySecondComponent;
