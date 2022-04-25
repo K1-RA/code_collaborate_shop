a4dc78ac31c4278b8c1d02e55d092b67c4fe31b3
fagerblom <nora.fagerblom@hyperisland.se>
2022-04-25 11:45:55 +0200

    more frontend start

--- components/index.js
+++ components/index.js
@@ -1,4 +1,3 @@
-
 export { default as Button } from './Button/Button';
 export { default as CartItem } from './CartItem/CartItem';
 export { default as Checkout } from './Checkout/Checkout';
@@ -6,4 +5,4 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Product } from './Product/Product';
 export { default as ProductsList } from './ProductsList/ProductsList';
 export { default as SummaryCard } from './SummaryCard/SummaryCard';
-
+export { default as Layout } from './Layout/Layout';
