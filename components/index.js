93b8bca605218c10dc069961c54b72e880dd55f9
Birk Kensén <birk.kensen@gmail.com>
2022-04-25 14:20:28 +0200

    Please

--- components/index.js
+++
@@ -1,9 +0,0 @@
-
-export { default as Button } from './Button/Button';
-export { default as CartItem } from './CartItem/CartItem';
-export { default as Checkout } from './Checkout/Checkout';
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Product } from './Product/Product';
-export { default as ProductsList } from './ProductsList/ProductsList';
-export { default as SummaryCard } from './SummaryCard/SummaryCard';
-
