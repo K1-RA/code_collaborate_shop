37376b7a19e0166ce19a31e0e779afb322fcbc41
Julia Wos <juliawos22@gmail.com>
2022-05-02 18:51:31 +0200

    fetch orders in admin page

--- pages/admin/orders/index.js
+++ pages/admin/orders/index.js
@@ -1,10 +1,37 @@
 import styles from './orders.module.scss';
+import { useState, useEffect } from 'react';
 
 const Orders = () => {
+	const [order, setOrders] = useState([]);
+
+	useEffect(() => {
+		fetch('http://localhost:8080/api/orders/')
+			.then((res) => res.json())
+			.then((json) => setOrders(json))
+			.catch((err) => console.log(err.message));
+	}, []);
+
+	console.log(order);
+
+	// 	useEffect(async () => {
+	// const data = await loadOrders()
+	// 	setOrders(data)
+
+	// 	}, [orders]);
+
+	// 	const loadOrders = async () => {
+	// 		const res = await fetch('http://localhost:8080/api/orders/');
+	// 		const data = await res.json();
+	// 		setOrders(data);
+	// 		console.log(order);
+
+	// 		return data;
+	// 	};
+
 	return (
 		<div>
 			<h1>Orders</h1>
-			<div>
+			{/* <div>
 				<table className={styles.table}>
 					<tr>
 						<td className={styles.tdOrders} colSpan='2'>
@@ -27,7 +54,7 @@ const Orders = () => {
 						</td>
 					</tr>
 				</table>
-			</div>
+			</div> */}
 		</div>
 	);
 };
